edc691ee5786d4b5b67db385e82d6cb61e1d02d1
Daniel Bonnauer <bonnauer@predic8.de>
2022-09-14 11:08:47 +0200

    initial commit

---
+++ apache-kafka-avro-schema-registry-demo/src/main/java/de/predic8/avroDemo/SimpleConsumer.java
@@ -0,0 +1,43 @@
+package de.predic8.avroDemo;
+
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+import static org.apache.kafka.clients.consumer.ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG;
+
+public class SimpleConsumer {
+
+
+    public static void main(String[] args) {
+
+        Properties props = new Properties();
+        props.put(BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        props.put("group.id", "article-consumer");
+        // always read from first
+        props.put("auto.commit.enable", "false");
+        props.put("auto.offset.reset", "earliest");
+
+        // avro
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "io.confluent.kafka.serializers.KafkaAvroDeserializer");
+        props.put("schema.registry.url", "http://localhost:8081");
+        props.setProperty("specific.avro.reader", "true");
+
+        try (KafkaConsumer<String, Article> consumer = new KafkaConsumer<>(props)) {
+            consumer.subscribe(Collections.singleton("articles"));
+
+            while (true) {
+                System.out.println("Polling messages.");
+                ConsumerRecords<String, Article> records = consumer.poll(Duration.ofSeconds(1));
+
+                records.forEach(record -> System.out.println(record.value().toString()));
+
+                consumer.commitSync();
+            }
+        }
+    }
+}
---
+++ apache-kafka-avro-schema-registry-demo/src/main/java/de/predic8/avroDemo/SimpleProducer.java
@@ -0,0 +1,33 @@
+package de.predic8.avroDemo;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+import static org.apache.kafka.clients.consumer.ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG;
+
+public class SimpleProducer {
+
+    public static void main(String[] args) {
+
+        Properties props = new Properties();
+        props.put(BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        props.put("acks", "all");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "io.confluent.kafka.serializers.KafkaAvroSerializer");
+        props.put("schema.registry.url", "http://localhost:8081");
+
+        System.out.println("Creating Message Producer");
+        try (Producer<String, Article> producer = new KafkaProducer<>(props)){
+
+            ProducerRecord<String, Article> record = new ProducerRecord<>("articles", "key",
+                    new Article("Salami", 5, "Deutschland"));
+
+            producer.send(record);
+
+            System.out.println("Message sent.");
+        }
+    }
+}
